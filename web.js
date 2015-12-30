42a0235695deb27cd70f67e548fb792a3575c2e8
Joshua Santos <nerdsville@nerdsville.net>
2015-12-30 08:18:16 -0800

    Initial Commit

---
+++ web.js
@@ -0,0 +1,25 @@
+var keystone = require('keystone');
+
+keystone.init({
+  'name': 'keystone-blog-demo-r66d',
+  'favicon': 'public/favicon.ico',
+  'less': 'public',
+  'static': ['public'],
+
+  'views': 'templates/views',
+  'view engine': 'jade',
+
+  'auto update': true,
+  'mongo': 'mongodb://localhost/keystone-blog-demo-r66d'
+
+  'session': true,
+  'auth': true,
+  'user model': 'User',
+  'cookie secret': 'th1sisad3m0'
+});
+
+require('./models');
+
+keystone.set('routes', require('./routes'));
+
+keystone.start();
